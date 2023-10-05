78f03357aced212f81922bffb9e4c4016586792e
Faiem-qy <f.quanyoung@gmail.com>
2023-10-04 21:48:56 -0400

    refactor getallreservations function to handle queries

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -70,9 +70,9 @@ const addUser = function(user) {
       `INSERT INTO users( name, email, password)
       VALUES ($1, $2, $3) RETURNING *;`,
       // Add RETURNING *; to the end of an INSERT query to return the objects that were inserted. This is handy when you need the auto generated id of an object you've just added to the database.
-      [user.name,user.email,user.password])
+      [user.name, user.email, user.password])
     .then((result) => {
-        return result.rows;
+      return result.rows;
     })
     .catch((err) => {
       console.log(err.message);
@@ -87,7 +87,32 @@ const addUser = function(user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(
+      `SELECT reservations.id, 
+      properties.title,
+      properties.parking_spaces, 
+      properties.number_of_bathrooms, 
+      properties.number_of_bedrooms,
+      properties.thumbnail_photo_url, 
+      properties.cost_per_night, 
+      reservations.start_date, 
+      reservations.end_date,avg(rating) as average_rating
+      FROM reservations
+      JOIN properties ON reservations.property_id = properties.id
+      JOIN property_reviews ON properties.id = property_reviews.property_id
+      WHERE reservations.guest_id = $1
+      GROUP BY properties.id, reservations.id
+      ORDER BY reservations.start_date
+      LIMIT $2;`,
+      [guest_id, limit])
+      .then((result) => {
+        console.log("getReservations ran");
+        return result.rows;
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
 };
 
 /// Properties
